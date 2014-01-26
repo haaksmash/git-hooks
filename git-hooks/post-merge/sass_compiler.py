4c4af38173c619208c19dfdc3dae36c9db23d64f
Haak Saxberg <haak.erling@gmail.com>
2014-01-26 01:19:45 -0800

    fixes sass_compiler mistakes

--- git-hooks/post-merge/sass_compiler.py
+++ git-hooks/post-merge/sass_compiler.py
@@ -3,17 +3,16 @@ import subprocess
 
 
 def compile_sass():
+    """compiles sass files into css"""
+
     sass_dir = os.environ.get("SASS_DIR", ".")
     css_dir = os.environ.get("CSS_DIR", ".")
-
-    """compiles sass files into css"""
     return subprocess.check_call(
-        "sass compile --sass-dir {} --css-dir {}".format(
-            sass_dir, css_dir,
-        )
+        ["sass" "--update", "{}:{}".format(sass_dir, css_dir)],
+    )
 
 if __name__ == "__main__":
     print "compiling sass to css..."
     if compile_sass():
         print "\tsomething went wrong!
-        return 1
+        1
